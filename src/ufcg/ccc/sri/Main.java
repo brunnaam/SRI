ff578e820c5331b2b0900d835ecc9df599d5804a
Brunna Amorim <brunnaamorim@gmail.com>
2016-07-11 00:03:20 -0300

    dictionary added

--- src/ufcg/ccc/sri/Main.java
+++ src/ufcg/ccc/sri/Main.java
@@ -3,41 +3,63 @@ package ufcg.ccc.sri;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+	
+	private static BTree<String, List<Integer>> btree = new BTree<String, List<Integer>>();
+	
+	private static void addToTree(String[] words, Integer docNumber) {
+		for (String word : words) {
+			if (btree.get(word) == null) {
+				List<Integer> docs = new ArrayList<Integer>();
+				docs.add(docNumber);
+				btree.put(word, docs);
+			} else {
+				List<Integer> docs = btree.get(word);
+				if (!docs.contains(docNumber)) {
+					docs.add(docNumber);
+				}
+			}
+		}
+	}	
 
-	public static void main(String[] args) {
-		
+	@SuppressWarnings("resource")
+	public static void fillDictionary(String filePath) {
 		try {
-			BufferedReader reader = new BufferedReader(new FileReader(new File("C:\\Users\\Brunna\\Downloads\\ptwiki-v2.trec")));
+			BufferedReader reader = new BufferedReader(new FileReader(new File(filePath)));
 			
 			String line = "";
-//			String text = "";
 			int docNumber = 1;
 			
 			while ((line = reader.readLine()) != null) {
-				if (line.contains("<DOC>")) {
-					System.out.println("------------- Inicio do Documento " + docNumber + " ------------ ");
-					System.out.println(line);
-				} else if (line.contains("</DOC>")) {
-					System.out.println(line);
-					System.out.println("------------- Fim do Documento " + docNumber + " ------------ ");
-//					System.out.println(text);
-//					String cleanText = TokenCleaner.clean(text);
-//					System.out.println(cleanText);
+				if (line.contains("</DOC>")) {
 					docNumber++;
-//					text = "";
-				} else if (line.contains("<P>") || line.contains("</P>")){
-					System.out.println(line);
-					String cleanText = TokenCleaner.clean(line);
-					System.out.println(cleanText);
-//					text += line + " ";
+				} else if (line.contains("</P>")){
+					String docLine = line.replaceAll("</P>", "");
+					String cleanText = TokenCleaner.clean(docLine);
+					String[] words = cleanText.split(" ");
+					addToTree(words, docNumber);
 				}
-			}       
+			}   
+			
+			System.out.println(btree);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		 
+	}
+
+	public static void search(String search) {
+		//TODO
+		String[] words = search.split(" ");
+	}
+	
+	public static void main(String[] args) {
+		
+		fillDictionary("C:\\Users\\Brunna\\Downloads\\ptwiki-v2.trec");
+				 
 	}
 
 }
