80bebf4a476259d689de10be4ebee95472cf9779
Brunna <Brunna>
2016-08-10 17:24:21 -0300

    checkpoint 3

--- src/ufcg/ccc/sri/lucene/Main.java
+++ src/ufcg/ccc/sri/lucene/Main.java
@@ -1,17 +1,51 @@
 package ufcg.ccc.sri.lucene;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import org.apache.lucene.queryparser.classic.ParseException;
 
 public class Main {
 	
-	public static void main(String[] args) throws IOException, ParseException {
-		Lucene l = new Lucene();
+	private static Lucene l;
+	
+	public static void main(String[] args) {
 		
-		l.readDocs("files/ptwiki-v2.trec");
-		l.luceneSearch("escritor inglês", 23);
+		try {
+			l = new Lucene();
+			l.readDocs("files/ptwiki-v2.trec");
+			
+			testQuery();
+//			userInputQuery();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 		
+	}
+	
+	public static void testQuery() throws IOException, ParseException {
+		System.out.println("Query: Escritor inglês \n ---- Top 10 results: ---- ");
+		l.luceneSearch("Escritor inglês", 10);
+	}
+	
+	@SuppressWarnings("resource")
+	public static void userInputQuery() throws IOException, ParseException {
+		
+		Scanner userInput = new Scanner(System.in);
+		String search = "";
+		
+		while (!search.equals("exit")) {
+			System.out.println("Digite sua busca: ");
+			search = userInput.nextLine();
+			if (search.equals("exit")) {
+				break;
+			}
+			System.out.println("Query: " + search + "\n ---- Top 10 results: ----") ;
+			l.luceneSearch(search, 10);
+		}
 	}
 
 }
